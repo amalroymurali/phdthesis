d4cb44c5808a9a9391b9dcc84c0c9ef2d67aa674
amalroymurali <roy.amalmurali@gmail.com>
2025-09-04 22:44:21 +0200

    Thesis Approved.

--- chapter09_figs/fetch.sh
+++
@@ -1,1 +0,0 @@
-rsync newton-haswell:/store/lmfa-2/mambo/amurali/scratch_backup/230310_VALIANT_LBM_ProLBv3/valiantlbm/results/AOAp180SDAp352SGPp079SOLn064/output/Dilatation.
--- compress.sh
+++
@@ -1,1 +0,0 @@
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=Thesis_Manuscript_ARM_final_compressed.pdf Thesis_Manuscript_ARM_final.pdf
