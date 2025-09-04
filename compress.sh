ee1a91bd6a579b0d100580672d23607a83747952
amalroymurali <roy.amalmurali@gmail.com>
2025-09-04 22:14:23 +0200

    Thesis approved

--- compress.sh
+++ compress.sh
@@ -1,1 +1,1 @@
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=Thesis_Manuscript_ARM_Compressed.pdf Thesis_Manuscript_ARM.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=Thesis_Manuscript_ARM_final_compressed.pdf Thesis_Manuscript_ARM_final.pdf
